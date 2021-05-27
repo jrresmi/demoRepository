7c33a4e7dffd2658f28a711981791ea4687287f8
ustjava17 <ustjava17@java29.iiht.tech>
2021-05-27 13:37:28 +0530

    msg

---
+++ DemoProject/src/com/MyClass.java
@@ -0,0 +1,11 @@
+package com;
+
+public class MyClass {
+int i=9;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcome to the project");
+int num;
+	}
+
+}
